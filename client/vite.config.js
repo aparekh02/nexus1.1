c15118d9d6213c72f189930f996d2ae871052710
Aksh Parekh <parekh.aksh.c@gmail.com>
2025-08-29 11:05:48 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,14 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'build'
   },
   server: {
     host: '0.0.0.0',
@@ -26,5 +29,3 @@ export default defineConfig({
     ]
   },
 })
-
-
