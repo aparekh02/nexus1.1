6943ec8152b00a038a02be9d0da86607b6bbee34
Aksh Parekh <parekh.aksh.c@gmail.com>
2025-08-29 10:10:38 -0700

    Update CreateProject.jsx

--- client/src/components/CreateProject.jsx
+++ client/src/components/CreateProject.jsx
@@ -18,7 +18,7 @@ const ACCESS_LEVELS = [
 ]
 
 // Set the base URL for your Flask backend
-const API_BASE_URL = 'http://localhost:5000'
+const API_BASE_URL = 'https://nexus-backend-f2td.onrender.com'
 axios.defaults.baseURL = API_BASE_URL
 
 export default function CreateProject({ onBack, onProjectCreated }) {
