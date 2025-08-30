7f5e0cf6c1a0bd043160f07563567f2524e7e514
Aksh Parekh <parekh.aksh.c@gmail.com>
2025-08-29 17:15:32 -0700

    configure_

--- client/src/components/ProjectFeed.jsx
+++ client/src/components/ProjectFeed.jsx
@@ -9,7 +9,7 @@ import { useAuth } from './AuthContext'
 const PROJECTS_PER_PAGE = 10;
 
 // Set the base URL for your Flask backend
-const API_BASE_URL = 'https://nexus-backend-f2td.onrender.com'
+const API_BASE_URL = 'https://nexus-backend-f2td.onrender.com/api'
 axios.defaults.baseURL = API_BASE_URL
 
 export default function ProjectFeed() {
